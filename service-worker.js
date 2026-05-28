2e7cb779967f662254f18468707864ddab3a070e
zhizhi67742 <xingmu1201@gmail.com>
2026-05-28 09:46:42 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,48 @@
+const CACHE_NAME = "pikminwish-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./favicon.ico",
+  "./icon-16.png",
+  "./icon-32.png",
+  "./icon-48.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open("pikminwish-v2").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
+  const req = event.request;
+
+  if (req.mode === "navigate" || req.destination === "document") {
+    event.respondWith(
+      fetch(req).catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(req)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+        return response;
+      })
+      .catch(() => caches.match(req))
   );
 });
